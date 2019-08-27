66feb4361e3632b44950b0466ceda29cd3638930
Lucas Lira <lucasgta95@gmail.com>
2019-08-27 19:53:18 -0300

    sei n mano

---
+++ resources/views/index.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        @include('meta_tags')
+        @include('css_imports')
+        <title>The Layout Trails</title>
+        <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @include('navbar')
+        
+        <div id="principal">The Layout Trails</div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        @include('js_imports')
+    </body>
+</html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark shadow sticky-top" style="background-color:#1ad64f;">
+    <a class="navbar-brand" id="logo_text" href="/">
+        <img id="logo_svg" src="{{ asset('svg/palette.svg') }}" width="30" height="30" class="d-inline-block align-top svg" alt="">
+        The Layout Trails
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Não sei ainda <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">FAQs</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="about">Sobre</a>
+            </li>
+        </ul>
+    </div>
+</nav>
