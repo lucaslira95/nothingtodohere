2fe096014666da134ad2a7f4554b9e213c88325e
Lucas Lira <lucasgta95@gmail.com>
2019-08-31 21:50:25 -0300

    pega no meu pau e cheira

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark shadow sticky-top" style="background-color:#1ad64f;">
     <a class="navbar-brand" id="logo_text" href="/">
         <img id="logo_svg" src="{{ secure_asset('svg/palette.svg') }}" width="30" height="30" class="d-inline-block align-top svg" alt="">
-        The Layout Trails
+        Nothing to do here
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
