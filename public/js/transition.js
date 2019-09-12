dfcce3620ff0449d5a400b8dfd3bf04e70ebadfa
Lucas Lira <lucasgta95@gmail.com>
2019-09-11 23:13:14 -0300

    fixed gradient repeating

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.load').click(function(event){
         event.preventDefault();
-        
+
         $('#dynamic').fadeOut('fast');
-        
+
 
         $('#dynamic').load($(this).attr('href'), function(){
             $('#dynamic').fadeIn('fast');
@@ -11,4 +11,4 @@ $(document).ready(function(){
 
         return false;
     });
-})
+});
